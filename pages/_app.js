2894e196a78f2d06c125ec7228265b4eec169152
Keisuke Watanuki <watanuki@ju-zen.com>
2022-06-01 01:52:05 +0900

    Add animation

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
+import { AnimatePresence } from "framer-motion";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <AnimatePresence><Component {...pageProps} /></AnimatePresence>
 }
 
 export default MyApp
